2689f36805bd0f4a113c77fcf530b08770168557
Jerome Van Der Linden <jeromevdl@gmail.com>
2012-09-07 13:35:29 +0200

    clean generated folder with cron

--- cleanup.php
+++ cleanup.php
@@ -1,12 +1,11 @@
 <?
-	$pwd = $_GET['pwd'];
-
-	if (!isset($pwd) || $pwd != "5yr6U84H91h48AO") {
-		die("Illegal access");	
-	} 
+	$time = microtime();
+	$time = explode(' ', $time);
+	$time = $time[1] + $time[0];
+	$begintime = $time;
 	
 	date_default_timezone_set('UTC');
-	$day_before = date("d") - 1;
+	$day_before = date("d") - 2;
 	if ($day_before < 10)
 		$day_before = "0".$day_before;
 		
@@ -16,7 +15,20 @@
 	if (file_exists($previous_folder)) {
 		rrmdir($previous_folder);
 	}
+	$time = microtime();
+	$time = explode(" ", $time);
+	$time = $time[1] + $time[0];
+	$endtime = $time;
+	$totaltime = ($endtime - $begintime);
 	
+	if (file_exists($previous_folder)) {
+		echo "Unable to delete ".$previous_folder. "(".$totaltime." seconds)";
+	} else {
+		echo $previous_folder." successfully deleted (".$totaltime." seconds)";
+	}
+	
+	// ========================= functions ====================================
+		
 	function rrmdir($dir) {
 	    foreach(glob($dir . '/*') as $file) {
 	        if(is_dir($file))
@@ -27,9 +39,4 @@
 	    rmdir($dir);
 	}
 	
-	if (file_exists($previous_folder)) {
-		echo "Unable to delete ".$previous_folder;
-	} else {
-		echo $previous_folder." successfully deleted";
-	}
 ?>
