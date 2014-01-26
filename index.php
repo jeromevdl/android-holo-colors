e62e1547d03af1202640d77823dc08dc236b99ea
Jerome Van Der Linden <jeromevdl@gmail.com>
2014-01-26 22:46:10 +0100

    reverse holo everywhere and compat

--- index.php
+++ index.php
@@ -1067,9 +1067,9 @@ var form = new studio.forms.Form('holocomponents', {
             buttons: true,
             options: [
                 { id: 'old', title: 'None' },
+                { id: 'holoeverywhere', title: 'HoloEverywhere' },
                 { id: 'abs', title: 'Sherlock' },
-                { id: 'compat', title: 'AppCompat' },
-                { id: 'holoeverywhere', title: 'HoloEverywhere' }
+                { id: 'compat', title: 'AppCompat' }
             ],
             defaultValue: 'compat'
         })),
@@ -1289,8 +1289,9 @@ var form = new studio.forms.Form('holocomponents', {
 
 form.createUI($('#inputs-form').get(0));
 </script>
-<a href="https://github.com/jeromevdl/android-holo-colors-idea-plugin" target="_blank" title="IntelliJ / Android Studio Plugin">
-<img style="position: absolute; top: 0; left: 0; border: 0;" src="plugin.png" alt="Available as IntelliJ plugin">
+<a href="https://github.com/jeromevdl/android-holo-colors-idea-plugin" target="_blank"
+   title="IntelliJ / Android Studio Plugin">
+    <img style="position: absolute; top: 0; left: 0; border: 0;" src="plugin.png" alt="Available as IntelliJ plugin">
 </a>
 <iframe id="frame_download" width="0" height="0" style="display:none"></iframe>
 </body>
